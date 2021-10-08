709077634103541f4fd659bc24b4895b2a703a22
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-08 18:07:08 +0330

    Ad, Advertiser and BaseModel class implemented.

--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    def get_clicks(self):
+        return self.clicks
+
+    def get_views(self):
+        return self.views
+
+    def inc_clicks(self):
+        self.clicks += 1
+
+    def inc_views(self):
+        self.views += 1
+
+
+class Advertiser(BaseModel):
+    name = models.CharField(max_lemgth=50)
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+
+class Ad(BaseModel):
+    title = models.CharField(max_length=50)
+    img_url = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
