6784c9b0e53c58bbc8515e2716637eca04fe8902
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-08 23:26:38 +0330

    show view added.

--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Ad
 
-# Create your views here.
+
+def show(request, ad_id):
+    template = loader.get_template('advertiser_management/templates/advertiser_management/ads.html')
+    ad = Ad.objects.get(pk=ad_id)
+    context = {
+        'ad': ad,
+        'advertiser': ad.advertiser_id,
+    }
+    return HttpResponse(template.rander(context, request))
