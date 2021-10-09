83ced7cfc83c5e788b1f5587f26c4b96e5b59667
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-09 12:17:29 +0330

    URL configurations set.

--- Yektanet/Yektanet/urls.py
+++ Yektanet/Yektanet/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('advertiser/', include('advertiser_management.urls')),
+    path('', include('advertiser_management.urls')),
     path('admin/', admin.site.urls),
 ]
--- Yektanet/advertiser_management/urls.py
+++ Yektanet/advertiser_management/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'advertiser_management'
 urlpatterns = [
     path('ads/', views.show, name='ads'),
+    path('advertise/', views.advertise, name='advertise'),
+    path('click/<int:ad_id>/', views.click, name='click'),
 ]
