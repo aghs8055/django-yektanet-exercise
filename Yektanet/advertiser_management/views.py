3859845194cef0341c7775615cfe82068624567c
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-09 12:16:22 +0330

    All specified views created.

--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,12 +1,49 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Advertiser
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Advertiser, Ad
+from django.shortcuts import render, redirect
+from .forms import AdForm
 
 
 def show(request):
-    template = loader.get_template('advertiser_management/templates/advertiser_management/ads.html')
     advertisers = Advertiser.objects.all()
+    for i in advertisers:
+        i.inc_views()
+        i.save()
+        i.ads = i.ad_set.all()
+        for j in i.ads:
+            j.inc_views()
+            j.save()
+            j.advertiser_id.save()
     context = {
         'advertisers': advertisers,
     }
-    return HttpResponse(template.rander(context, request))
+    return render(request, 'advertiser_management/ads.html', context)
+
+
+def advertise(request):
+    if request.method == 'POST':
+        form = AdForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            img_url = form.cleaned_data['img_url']
+            link = form.cleaned_data['link']
+            try:
+                advertiser_id = Advertiser.objects.get(pk=form.cleaned_data['advertiser_id'])
+            except (KeyError, Advertiser.DoesNotExist):
+                return render(request, 'advertiser_management/advertise.html',
+                              context={'form': form, 'error_message': 'Advertiser not found!'})
+            ad = Ad(title=title, link=link, img_url=img_url, advertiser_id=advertiser_id)
+            ad.save()
+            return HttpResponseRedirect(reverse('advertiser_management:ads'))
+    else:
+        form = AdForm()
+        return render(request, 'advertiser_management/advertise.html', {'form': form})
+
+
+def click(request, ad_id):
+    ad = Ad.objects.get(pk=ad_id)
+    ad.inc_clicks()
+    ad.save()
+    ad.advertiser_id.save()
+    return redirect(ad.link)
