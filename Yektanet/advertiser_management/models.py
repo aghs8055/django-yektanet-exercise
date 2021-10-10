2d0ad622ee53201a0f0f94df5f2f1d3ffbd22e73
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-10 21:47:15 +0330

    Click and View model added.

--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -1,27 +1,7 @@
 from django.db import models
 
 
-class BaseModel(models.Model):
-    clicks = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
-
-    def get_clicks(self):
-        return self.clicks
-
-    def get_views(self):
-        return self.views
-
-    def inc_clicks(self):
-        self.clicks += 1
-
-    def inc_views(self):
-        self.views += 1
-
-    class Meta:
-        abstract = True
-
-
-class Advertiser(BaseModel):
+class Advertiser(models.Model):
     name = models.CharField(max_length=50)
 
     def get_name(self):
@@ -30,15 +10,11 @@ class Advertiser(BaseModel):
     def set_name(self, name):
         self.name = name
 
-    @staticmethod
-    def get_total_clicks():
-        return sum([advertiser.clicks for advertiser in Advertiser.objects.all()])
-
     def __str__(self):
         return self.name
 
 
-class Ad(BaseModel):
+class Ad(models.Model):
     title = models.CharField(max_length=50)
     img_url = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
@@ -47,10 +23,22 @@ class Ad(BaseModel):
     def __str__(self):
         return self.title
 
-    def inc_clicks(self):
-        super().inc_clicks()
-        self.advertiser_id.inc_clicks()
 
-    def inc_views(self):
-        super().inc_views()
-        self.advertiser_id.inc_views()
+class BaseModel(models.Model):
+    ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    ip = models.GenericIPAddressField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.ad_id
+
+
+class Click(BaseModel):
+    pass
+
+
+class View(BaseModel):
+    pass
