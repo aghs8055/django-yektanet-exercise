425557480e2d99135527d966e96d602842ab801e
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-11 23:51:56 +0330

    Admin site changed to be used for approving ads and edit or add ads and advertisers.

--- Yektanet/advertiser_management/admin.py
+++ Yektanet/advertiser_management/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import Advertiser
+from .models import *
+
+
+def approve(modeladmin, request, queryset):
+    queryset.update(approve=True)
+
+
+def reject(modeladmin, request, queryset):
+    queryset.update(approve=False)
+
+
+approve.short_description = 'Approve selected ads'
+reject.short_description = 'Reject selected ads'
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_filter = ('approve',)
+    list_display = ('title', 'approve')
+    actions = [approve, reject]
+
 
 admin.site.register(Advertiser)
