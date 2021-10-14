cbf55666cc72bd3de2c1841b6303e02a0175bf68
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-14 10:41:54 +0330

    Query for specified reports.

--- Yektanet/advertiser_management/urls.py
+++ Yektanet/advertiser_management/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'advertiser_management'
 urlpatterns = [
     path('advertise/', views.AdvertiseView.as_view(), name='advertise'),
     path('click/<int:ad_id>/', views.ClickAdView.as_view(), name='click'),
-    path('ads/', views.ShowAdsView.as_view(), name='show')
+    path('ads/', views.ShowAdsView.as_view(), name='show'),
+    path('report/', views.ReportView.as_view(), name='Report')
 ]
--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from .models import *
-from django.shortcuts import render, redirect
-from .forms import AdForm
 from django.utils import timezone
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView, RedirectView, DetailView
 from django.views.generic.edit import CreateView
+from django.db.models import *
+from datetime import datetime
 
 
 class ShowAdsView(TemplateView):
@@ -33,3 +31,47 @@ class ClickAdView(RedirectView):
         ad = Ad.objects.get(pk=kwargs['ad_id'])
         Click(ad_id=ad, datetime=timezone.now(), ip=self.request.META['REMOTE_ADDR']).save()
         return ad.link
+
+
+class ReportView(TemplateView):
+    template_name = 'advertiser_management/total_clicks_views.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ads = Ad.objects.all()
+        total = {ad: [] for ad in Ad.objects.all()}
+        for i in range(24):
+            for ad in ads:
+                total[ad].append(
+                    (i, i + 1,
+                     ad.click_set.filter(datetime__hour=i).count() + ad.view_set.filter(datetime__hour=i).count()))
+        context['total'] = total
+        rate = {ad: [] for ad in Ad.objects.all()}
+        for i in range(24):
+            for ad in ads:
+                if ad.view_set.filter(datetime__hour=i).count() != 0:
+                    rate[ad].append(
+                        (i, i + 1,
+                         ad.click_set.filter(datetime__hour=i).count() / ad.view_set.filter(datetime__hour=i).count()))
+        for ad in ads:
+            rate[ad].sort(key=lambda x: -x[2])
+        context['rate'] = rate
+        views = View.objects.all()
+        click_time, view_time, view_count = 0, 0, 0
+        for view in views:
+            click_times = []
+            view_time += view.datetime.timestamp()
+            clicks = Click.objects.filter(ad_id=view.ad_id, datetime__gt=view.datetime, ip=view.ip)
+            for click in clicks:
+                click_times.append(click.datetime.timestamp())
+            if len(click_times) != 0:
+                click_time += sum(click_times) / len(click_times)
+                view_count +=1
+            else:
+                view_time -= view.datetime.timestamp()
+        if len(views) != 0:
+            context['click_delay'] = (click_time - view_time) / view_count
+        else:
+            context['click_delay'] = 0
+
+        return context
